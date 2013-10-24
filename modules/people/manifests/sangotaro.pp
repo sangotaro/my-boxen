2e7f724180e55d2ad6a58287ff5b6ce7e6004be9
sangotaro <sango0715@gmail.com>
2013-10-25 02:00:09 +0900

    tree and rename command

--- modules/people/manifests/sangotaro.pp
+++ modules/people/manifests/sangotaro.pp
@@ -2,5 +2,12 @@ class people::sangotaro {
   include virtualbox
   include vagrant
   include firefox
+
+  package {
+    [
+      'tree',
+      'rename'
+    ]:
+  }
   $home = "/Users/$(::boxen_user)"
 }
