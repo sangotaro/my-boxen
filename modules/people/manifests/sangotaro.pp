7dce897c04b8921ea7ef8388b423f16df7ca8e36
sangotaro <sango0715@gmail.com>
2013-10-28 22:38:48 +0900

    dotfilesのインストール

--- modules/people/manifests/sangotaro.pp
+++ modules/people/manifests/sangotaro.pp
@@ -9,5 +9,17 @@ class people::sangotaro {
       'rename'
     ]:
   }
-  $home = "/Users/$(::boxen_user)"
+
+  # dotfile setting
+  $home = "/Users/${::boxen_user}"
+  $dotfiles = "${home}/dotfiles"
+
+  repository { $dotfiles:
+    source => 'sangotaro/dotfiles',
+  }
+  exec { "sh ${dotfiles}/install.sh":
+    cwd => "${home}/dotfiles",
+    require => Repository[$dotfiles]
+  }
+
 }
