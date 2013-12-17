1d7d2f26155efa0895cc6684a9a9bc163cd946cc
sangotaro <sango0715@gmail.com>
2013-12-17 17:59:58 +0900

    refactoring

--- modules/people/manifests/sangotaro.pp
+++ modules/people/manifests/sangotaro.pp
@@ -4,6 +4,18 @@ class people::sangotaro {
   include chrome
   include firefox
 
+  package {
+    'XtraFinder':
+      source   => "http://www.trankynam.com/xtrafinder/downloads/XtraFinder.dmg",
+      provider => pkgdmg;
+    'GoogleJapaneseInput':
+      source => "http://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg",
+      provider => pkgdmg;
+    'Mou':
+      source   => "http://mouapp.com/download/Mou.zip",
+      provider => compressed_app;
+  }
+
   # Homebrew
   homebrew::tap { 'sanemat/font': }
   package { 'ricty': }
@@ -15,11 +27,10 @@ class people::sangotaro {
     ]:
   }
 
-
-  # dotfile setting
   $home = "/Users/${::boxen_user}"
   $dotfiles = "${home}/dotfiles"
 
+  # dotfiles
   repository { $dotfiles:
     source => 'sangotaro/dotfiles',
   }
@@ -27,18 +38,5 @@ class people::sangotaro {
     cwd => "${home}/dotfiles",
     require => Repository[$dotfiles],
     creates => "${home}/.vimrc"
-  }
-
-  # other
-  package {
-    'XtraFinder':
-      source   => "http://www.trankynam.com/xtrafinder/downloads/XtraFinder.dmg",
-      provider => pkgdmg;
-    'GoogleJapaneseInput':
-      source => "http://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg",
-      provider => pkgdmg;
-    'Mou':
-      source   => "http://mouapp.com/download/Mou.zip",
-      provider => compressed_app;
   }
 }
