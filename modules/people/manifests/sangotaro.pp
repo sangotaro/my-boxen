e8ea0a5747e7ec09074e81eb4f3dd1a31ecc632a
sangotaro <sango0715@gmail.com>
2014-04-23 11:07:28 +0900

    Update

--- modules/people/manifests/sangotaro.pp
+++ modules/people/manifests/sangotaro.pp
@@ -1,20 +1,9 @@
 class people::sangotaro {
   include virtualbox
   include vagrant
-  include chrome
-  include firefox
 
   include people::sangotaro::nodejs
   include people::sangotaro::ruby
-
-  package {
-    'XtraFinder':
-      source   => "http://www.trankynam.com/xtrafinder/downloads/XtraFinder.dmg",
-      provider => pkgdmg;
-    'GoogleJapaneseInput':
-      source => "http://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg",
-      provider => pkgdmg;
-  }
 
   $home = "/Users/${::boxen_user}"
   $dotfiles = "${home}/dotfiles"
